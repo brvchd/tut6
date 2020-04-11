24da6b0f12e28af191c0981357b7d31033d17b61
Dmytro Borovych <s18963@pjwstk.edu.pl>
2020-04-11 14:04:44 +0200

    Tasks are completed

---
+++ Middleware/LoggingMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tut6.Services;
+
+namespace tut6.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext, IDbService dbSerivce)
+        {
+            string path = httpContext.Request.Path;
+            string queryString = httpContext.Request?.QueryString.ToString();
+            string method = httpContext.Request.Method.ToString();
+            string bodyParameters = String.Empty;
+
+            using(var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8, true ))
+            {
+                bodyParameters = await reader.ReadToEndAsync();
+            }
+
+            var LogWriter = new FileStream("requestLog.txt", FileMode.Create);
+            using(var writer = new StreamWriter(LogWriter))
+            {
+                string text = $"Path: {path} \nQueryString:{queryString} \nMethod: {method} \nBody Parameters: {bodyParameters}";
+                writer.WriteLine(text);
+            }
+
+            await _next(httpContext);
+        }
+
+    }
+}
+ 
---
+++ Services/SqlServerDbService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tut6.Services
+{
+    public class SqlServerDbService : IDbService
+    {
+        public bool CheckIndex(string index)
+        {
+            using (var con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s18963;Integrated Security=True"))
+            {
+                using (var com = new SqlCommand())
+                {
+                    com.Connection = con;
+                    com.CommandText = "Select IndexNumber from Student Where IndexNumber = @index";
+                    com.Parameters.AddWithValue("index", index);
+
+                    con.Open();
+                    var dr = com.ExecuteReader();
+                    return dr.Read();
+                }
+            }
+        }
+    }
+}
+
